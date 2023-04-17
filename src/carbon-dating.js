c9fc5b14c3d910a649c1f8b255a73bfcdf74d4aa
Ilkham-Karimzhan <ilhamkar00@gmail.com>
2023-04-18 00:44:03 +0600

    complete task 1 (Tue, Apr 18, 2023 12:44:03 AM)

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,23 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError("Not implemented");
-  // remove line with error and write your code here
+
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== "string" || isNaN(parseFloat(sampleActivity))) {
+    return false;
+  }
+
+  const activity = parseFloat(sampleActivity);
+
+  if (activity <= 0 || activity > MODERN_ACTIVITY) {
+    return false;
+  }
+
+  const age = Math.ceil(
+    (Math.log(MODERN_ACTIVITY / activity) * HALF_LIFE_PERIOD) / 0.693
+  );
+
+  return age;
 }
 
 module.exports = {
